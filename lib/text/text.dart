ca4d7448445407f318a421160efd1f1e6ec6f609
Shuvon-majhi <shuvon.majhi@gmail.com>
2023-12-15 14:02:04 +0600

    color

--- lib/text/text.dart
+++ lib/text/text.dart
@@ -6,7 +6,7 @@ class textW extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepPurple.shade100,
+      backgroundColor: Color(0xfff0dfea),
       body: Center(
         child: Text(
             "I am writing to inform you that SHUVON MAJHI will attend the Master of Business Administration (MBA) program at BAU in the USA starting on Summer, and I am delighted to confirm that all the student’s expenses and/or fees will be financed by me SHEFALI MAJHI (Mother) during their stay in the United States.",
